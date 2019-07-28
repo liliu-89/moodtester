7a6bee68416eebed7ca4f9b5ad26a98d3c6191df
smartninja-li <52677222+smartninja-li@users.noreply.github.com>
2019-07-28 11:54:05 +0200

    Add files via upload
    created moodtester

---
+++ moodtester.py
@@ -0,0 +1,14 @@
+mood = input("Enter your mood: ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Cheer up, mate!")
+elif mood == "excited":
+    print("Calm down, Bro!")
+elif mood == "relaxed":
+    print("Nice to hear")
+else:
+    print("I don't recognize this mood")
